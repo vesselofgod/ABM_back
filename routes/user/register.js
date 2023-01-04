7ab1052c6e952125ad179b9a301c67a1e0afdc68
vesselofgod <vesselofgod@naver.com>
2023-01-04 16:33:43 +0900

    fix: Divide ID duplicate confirmation part

--- routes/user/register.js
+++ routes/user/register.js
@@ -11,33 +11,41 @@ router.get("/", (req, res) => {
     res.send("register page")
 })
 
+router.post("/checkUserIdExist", async (req,res) => {
+    const uid = req.body.uid;
+    let user = await User.findOne({ uid });
+    if (user) {
+        return res.status(400).json({ 
+            errors: [{ msg: "User already exists" }],
+            duplication:true
+        });
+    };
+    return res.status(200).json({
+        duplication: false
+    });
+})
+
 
 router.post("/", async (req, res) => {
-    const { name, email, password } = req.body;
+    const { uid, password, name, sex, birth, email} = req.body;
 
     try {
         // email을 비교해서 user가 이미 존재하는지 확인
         // 존재한다면 return해서 뒤의 코드를 실행하지 않음.
-        let user = await User.findOne({ email });
+        let user = await User.findOne({ uid });
         if (user) {
             return res.status(400).json({ errors: [{ msg: "User already exists" }] });
         };
 
         // user가 존재하지 않으면 새로운 user에 대해서 DB에 추가
         user = new User({
-            name,
-            email,
+            uid,
             password,
+            name,
+            sex,
+            birth,
+            email
         });
-
-        
-
-        // bcrypt 모듈을 이용해 salt값을 부여하며 password 암호화
-        //const salt = await bcrypt.genSalt(10);
-        //user.password = await bcrypt.hash(password, salt);
-
-        // 암호화된 내용까지 포함해 DB에 user를 저장.
-        //await user.save();
 
         await user.save((err, doc)=>{
             if(err) return res.json({
