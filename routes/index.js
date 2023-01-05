183a75c9a268ee974cb7d7c89c2cb26ef7f4f1f0
vesselofgod <vesselofgod@naver.com>
2023-01-05 11:18:11 +0900

    doc: Writing a registration API document using a swagger

--- routes/index.js
+++ routes/index.js
@@ -14,11 +14,148 @@ const login = require("./user/login.js");
  */
 router.use("/user", user)
 
+
+
 /**
  * @swagger
  * tags:
  *   name: register
  *   description: 유저 추가
+* paths:
+*  /api/register:
+*   get:
+*     tags: [register]
+*     summary: 회원가입 페이지
+*     responses:
+*       "200":
+*         description: 회원가입 페이지 로드 성공
+*   post:
+*    summary: "회원가입 요청"
+*    description: "PUT 방식을 통해 유저 수정(전체 데이터를 수정할 때 사용함)"
+*    tags: [register]
+*    requestBody:
+*      description: 유저 수정
+*      required: true
+*      content:
+*        application/json:
+*          schema:
+*            type: object
+*            example:
+*              {
+*                 "uid":"abcd",
+*                 "password":"efgh123!",
+*                 "name":"qwer",
+*                 "sex":"Male",
+*                 "birth":"1999-04-03",
+*                 "email":"abcd@gmail.com"
+*              }
+*            properties:
+*              uid:
+*                type: string
+*                description: "유저 고유아이디"
+*              password:
+*                type: string
+*                description: "유저 비밀번호"
+*              name:
+*                type: string
+*                description: "유저 이름"
+*              sex:
+*                type: string
+*                description: "유저 성별 : 남자:M 여자:F"
+*              birth:
+*                type: date
+*                description: "유저의 생년월일"
+*              email:
+*                type: string
+*                description: "유저 이메일"
+*    responses:
+*      "200":
+*        description: 정상적으로 회원가입이 완료되었을 때.
+*        content:
+*          application/json:
+*            schema:
+*              type: object
+*              properties:
+*                success:
+*                  type: boolean
+*                  example: true
+*      "400":
+*        description: id가 중복되었을때
+*        content:
+*          application/json:
+*            schema:
+*              type: object
+*              properties:
+*                success:
+*                  type: boolean
+*                  example: false
+*                error:
+*                  type: object
+*                  example: { msg: "User already exists" }
+*      "500":
+*        description: server error가 발생했을 때.
+*        content:
+*          application/json:
+*            schema:
+*              type: object
+*              properties:
+*                success:
+*                  type: boolean
+*                  example: false
+*                error:
+*                  type: object
+*                  example: { msg: "TypeError: Cannot read properties of undefined" }
+*
+*/
+
+/**
+* @swagger
+* paths:
+*  /api/register/checkUserIdExist:
+*   post:
+*    summary: "유저 ID 중복확인요청"
+*    description: "유저 ID 중복확인요청"
+*    tags: [register]
+*    requestBody:
+*      description: "중복을 확인할 유저 ID "
+*      required: true
+*      content:
+*        application/json:
+*          schema:
+*            type: object
+*            example:
+*              {
+*                 "uid":"abcd",
+*              }
+*            properties:
+*              uid:
+*                type: string
+*                description: "유저 고유아이디"
+*    responses:
+*      "200":
+*        description: 정상적으로 회원가입이 완료되었을 때.
+*        content:
+*          application/json:
+*            schema:
+*              type: object
+*              properties:
+*                duplication:
+*                  type: boolean
+*                  example: false
+*      "400":
+*        description: id가 중복되었을때
+*        content:
+*          application/json:
+*            schema:
+*              type: object
+*              properties:
+*                duplication:
+*                  type: boolean
+*                  example: true
+*                error:
+*                  type: object
+*                  example: { msg: "User already exists" }
+*
  */
 router.use("/register", register)
 
@@ -27,6 +164,7 @@ router.use("/register", register)
  * tags:
  *   name: auth
  *   description: 유저 token 인증
+ * 
  */
 router.use("/auth",auth)
 
--- swagger.js
+++ swagger.js
@@ -1,27 +1,26 @@
 // swagger.js
-const swaggerUi = require('swagger-ui-express');
-const swaggereJsdoc = require('swagger-jsdoc');
+const swaggerUi = require("swagger-ui-express");
+const swaggereJsdoc = require("swagger-jsdoc");
 
 const options = {
-    swaggerDefinition: {
-      openapi: "3.0.0",
-      info: {
-        version: "1.0.0",
-        title: "ABM Swagger RestFul API UI",
-        description:
-          "Node.js RestFul API UI",
-      },
-      servers: [
-        {
-          url: "http://localhost:3000", // 요청 URL
-        },
-      ],
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      version: "1.0.0",
+      title: "ABM Swagger RestFul API UI",
+      description: "Node.js RestFul API UI",
     },
-    apis: ["./routes/*.js", "./routes/user/*.js"], //Swagger 파일 연동
-  }
+    servers: [
+      {
+        url: "http://localhost:3000", // 요청 URL
+      },
+    ],
+  },
+  apis: ["./routes/*.js", "./routes/user/*.js"], //Swagger 파일 연동
+};
 const specs = swaggereJsdoc(options);
 
 module.exports = {
-    swaggerUi,
-    specs
+  swaggerUi,
+  specs,
 };
