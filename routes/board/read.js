e01c1f2a41baf529476da15028c89ad812435eb2
vesselofgod <vesselofgod@naver.com>
2023-01-18 16:31:44 +0900

    feat: Add a isScrapped and matchStatus info in feed detail page

--- routes/board/read.js
+++ routes/board/read.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const Feed = require("../../model/feed");
 const Image = require("../../model/image");
 const Scrap = require("../../model/scrap");
+const Match = require("../../model/match");
 const utils = require("../../utils.js");
 
 router.get("/", async (req, res) => {
@@ -38,25 +39,40 @@ router.get("/recruit", async (req, res) => {
 
 router.get("/:feed_id", async (req, res) => {
   //각각의 게시판에 대한 상세한 정보를 제공함.
-  //토큰 받아와서 유저 확인하고 지역, 관심 카테고리를 알게 된다면
+  //토큰 받아와서 유저 확인
   try {
     const token = req.header("authorization").split(" ")[1];
     const user_data = utils.parseJWTPayload(token);
     const fid = req.params.feed_id;
     let isAuthor = false;
+    let isScrapped = false;
+    let matchStatus;
+
     let feed = await Feed.findOne({
       fid: fid,
       state: "Recruiting",
     });
+    let scrap = await Scrap.findOne({
+      fid: fid,
+      user: user_data.user.nickname,
+    });
+    let match = await Match.findOne({
+      fid: fid,
+      app_user: user_data.user.nickname,
+    });
+
     if (user_data.user.nickname == feed.author) isAuthor = true;
+    if (scrap != null) isScrapped = true;
+    if (match != null) matchStatus = match.accept;
 
     const images = await Image.find({ fid: fid });
-
     return res.status(200).json({
       success: true,
       feed: feed,
       images: images,
       isAuthor: isAuthor,
+      isScrapped: isScrapped,
+      matchStatus: matchStatus,
     });
   } catch (err) {
     console.log(err);
