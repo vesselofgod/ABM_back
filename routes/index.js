103b608c0b9ebc58a859da497879a60d1a8260e2
vesselofgod <vesselofgod@naver.com>
2023-01-18 15:08:52 +0900

    doc: Write about scrap delete API document

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ const createpage = require("./board/create.js");
 const readpage = require("./board/read.js");
 const deletepage = require("./board/delete.js");
 const updatepage = require("./board/update.js");
-const match = require("./board/match.js")
+const match = require("./board/match.js");
+const scrap = require("./board/scrap.js");
 
 router.use("/register", register);
 router.use("/auth", auth);
@@ -20,13 +21,12 @@ router.use("/read", readpage);
 router.use("/delete", deletepage);
 router.use("/update", updatepage);
 router.use("/match", match);
+router.use("/scrap", scrap);
 
 //need to change
 const uploadController = require("./user/upload");
 router.use("/upload", uploadController.uploadFiles);
 router.use("/files", uploadController.getListFiles);
 router.use("/files:name", uploadController.download);
-
-
 
 module.exports = router;
