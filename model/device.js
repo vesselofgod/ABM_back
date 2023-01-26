7cf7d8b13d3d40acd52a8bf4974a183d62907bab
vesselofgod <vesselofgod@naver.com>
2023-01-26 16:01:02 +0900

    feat: Add a device table and Change device token at login

---
+++ model/device.js
@@ -0,0 +1,19 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const deviceSchema = mongoose.Schema({
+  device_token: {
+    type: String,
+  },
+  uid: {
+    type: Number,
+  },
+  tokenExp:{
+    type: Date,
+  }
+});
+
+// 데이터베이스 모델을 정의
+const device = mongoose.model("device", deviceSchema);
+
+module.exports = device;
--- routes/user/login.js
+++ routes/user/login.js
@@ -1,12 +1,18 @@
 // express 모듈 내의 Router를 이용해서 회원정보를 post 방식으로 요청 받으면 DB에 회원정보 저장
 const express = require("express");
 const User = require("../../model/user");
+const Device = require("../../model/device")
 const router = express.Router();
-const jwt = require("jsonwebtoken");
+const admin = require("../../config/notice.config");
+const {JWT} = require("google-auth-library")
 require("dotenv").config();
+/*
+TODO: 로그인 시에 device token을 받은 다음에 device table에 저장한다.
+만약 해당 device token을 사용하고 있는 table이 발견된다면 그 테이블을 지우고 새로 table을 만든다.
 
+*/
 router.post("/", async (req, res) => {
-  const { uid, password } = req.body;
+  const { uid, password, device_token } = req.body;
   try {
     // id를 비교해서 user가 이미 존재하는지 확인
     // 존재한다면 return해서 뒤의 코드를 실행하지 않음.
@@ -28,7 +34,7 @@ router.post("/", async (req, res) => {
       //matched, token generation.
 
       //토큰 생성하는 부분에서 에러 발생
-      user.generateToken((err) => {
+      user.generateToken(async (err) => {
         if (err) return res.status(401).send(err);
         //토큰을 쿠키에 저장.
         let setProfile = null;
@@ -37,6 +43,25 @@ router.post("/", async (req, res) => {
         } else {
           setProfile = true;
         }
+        
+        await Device.deleteOne({
+          device_token: device_token,
+        });
+
+        let device = new Device({
+          device_token: device_token,
+          uid: uid,
+          //TODO : token 만료일 추가
+        });
+    
+        await device.save((err, doc) => {
+          if (err)
+            return res.status(401).json({
+              success: false,
+              err: "device token was not stored.",
+            });
+        });
+
         res.cookie("x_auth_token", user.token).status(200).json({
           loginSuccess: true,
           token: user.token,
