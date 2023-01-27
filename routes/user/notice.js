7a36d0761d69e062c77041e0bf1fc377fa79f7a9
vesselofgod <vesselofgod@naver.com>
2023-01-27 10:54:12 +0900

    feat: Add a API for updataTimestamp of device token

--- routes/user/notice.js
+++ routes/user/notice.js
@@ -18,6 +18,8 @@ TODO : notice쪽에서는 알림이 발생했을 경우에 user token 정보를 
 통해서 알림메시지를 보낸다.
 
 알림메시지에 대해서도 발생 원인에 따라서 chat, match등으로 나뉘어야 하고, 메시지 form도 있으면 좋을 것 같다.
+토큰 업데이트하는 API 추가
+여러개의 토큰 알림 테스트
 */
 
 function getAccessToken() {
@@ -57,8 +59,8 @@ router.post("/", async function (req, res, next) {
   } else if (device_tokens.length == 1) {
     let message = {
       notification: {
-        title: '테스트 발송',
-        body: '앱 확인해보세요!',
+        title: "테스트 발송",
+        body: "앱 확인해보세요!",
       },
       data: {
         title: "테스트 데이터 발송",
@@ -84,7 +86,6 @@ router.post("/", async function (req, res, next) {
           success: false,
         });
       });
-
   } else {
     let message = {
       data: {
@@ -95,7 +96,8 @@ router.post("/", async function (req, res, next) {
       token: device_tokens,
     };
 
-    getMessaging()
+    admin
+      .messaging()
       .sendMulticast(message)
       .then((response) => {
         if (response.failureCount > 0) {
@@ -108,6 +110,48 @@ router.post("/", async function (req, res, next) {
           console.log("List of tokens that caused failures: " + failedTokens);
         }
       });
+  }
+});
+
+router.patch("/updateTimestamp", async (req, res) => {
+  try {
+    const token = req.header("authorization").split(" ")[1];
+    const user_data = utils.parseJWTPayload(token);
+    const device_token = req.body.device_token;
+    let old_token = await Device.findOne({
+      uid: user_data.user.uid,
+      device_token: device_token,
+    });
+
+    if (old_token == null) {
+      let device = new Device({
+        device_token: device_token,
+        uid: uid,
+        timestamp: new Date(),
+      });
+
+      await device.save((err, doc) => {
+        if (err) {
+          return res.status(401).json({
+            success: false,
+            err: "device token was not stored.",
+          });
+        }
+        return res.status(200).json({ success: true });
+      });
+    }
+
+    let result = await Device.updateOne(
+      { device_token: device_token },
+      { timestamp: new Date() }
+    );
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
   }
 });
 
