38acf5965b4a6146735a8a75df1e7d2f657185c8
vesselofgod <vesselofgod@naver.com>
2023-01-27 10:52:55 +0900

    feat: Add a Device table to send notifications to each device

--- model/device.js
+++ model/device.js
@@ -1,4 +1,3 @@
-// models/User.js
 const mongoose = require("mongoose");
 
 const deviceSchema = mongoose.Schema({
@@ -8,9 +7,10 @@ const deviceSchema = mongoose.Schema({
   uid: {
     type: String,
   },
-  tokenExp:{
+  timestamp:{
+    //일정 시간동안 timestamp가 업데이트되지 않았다면 device token을 다시 받음
     type: Date,
-  }
+  },
 });
 
 // 데이터베이스 모델을 정의
