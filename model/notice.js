04cbb91b71e2d4b47ab2a1ad6f9dd58e7472529f
vesselofgod <vesselofgod@naver.com>
2023-01-30 14:11:20 +0900

    feat: Add a notice  to store notice message

---
+++ model/notice.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const noticeSchema = mongoose.Schema({
+  user: {
+    type: String,
+  },
+  title:{
+    type: String,
+  },
+  body:{
+    type: String,
+  },
+  isread:{
+    type: Boolean,
+    default: false,
+  },
+  link: {
+    //이 정보를 어디서 받은건지 구분할 수 있는 필드가 있어야함.
+    //match일 경우 fid를, chat일 경우 rid를 제공한다.
+    type: String,
+  },
+  type: {
+    //매치 성사알림일 경우 type은 match,
+    //채팅 알림일 경우 type은 chat
+    type: String,
+  }
+});
+
+const notice = mongoose.model("notice", noticeSchema);
+
+module.exports = notice;
