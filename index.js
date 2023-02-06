1e9e28fe95be7100acb59bea6e07087faf9c9926
vesselofgod <vesselofgod@naver.com>
2023-02-06 15:24:53 +0900

    feat: Add a leave_room in socket connection

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
 const cookieParser = require("cookie-parser");
-const { Server } = require("socket.io")
+const { Server } = require("socket.io");
 const http = require("http");
 const Message = require("./model/message");
 
@@ -52,29 +52,40 @@ app.use(
 );
 
 //socket
+function countRoom(roomName) {
+  // 방에 사람이 몇명이 있는지 계산하는 함수(set의 size를 이용)
+  return io.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 io.on("connection", (socket) => {
   console.log("user connected");
 
-  socket.on("enter_room", (data) => {
+  socket.on("join_room", (data) => {
+    if (countRoom(data.room) == 0) {
+      //방이 처음 만들어짐.
+      //피드와 연결해서 초기 톡방 설정 구현
+    }
     socket.join(data.room);
     socket.to(data.room).emit("welcome", data);
-    console.log(socket.rooms); //현재 들어가있는 방을 표시 (기본적으로 User와 Server 사이에 private room이 1개 있음)
+    //현재 들어가있는 방을 표시 (기본적으로 User와 Server 사이에 private room이 1개 있음)
+    console.log(socket.rooms);
   });
 
-  socket.on("welcome", (data) => {
-    console.log("welcome socket")
-    console.log(data)
+  socket.on("leave_room", (data) => {
+    socket.to(data.room).emit("bye", data);
+    socket.leave(data.room);
+    console.log(socket.rooms);
   });
 
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) => socket.to(room).emit("bye"));
   });
 
-  //socket.emit : 앱에서 기존 메시지를 읽을 수 있도록 서버가 채팅 기록을 불러와야 함. (emit : send의 역할을 함)  
+  //socket.emit : 앱에서 기존 메시지를 읽을 수 있도록 서버가 채팅 기록을 불러와야 함. (emit : send의 역할을 함)
   //socket.on("message" : 내가 앱에서 채팅을 쳤을 때 (on : receive의 역할을 함.)
   socket.on("new_message", async (data) => {
     // data : 각종 정보를 담고 있는 payload object
-    // data.room message를 보내는 room의 이름(귓을 위해서는 socket_id를 room으로 사용하면 된다.)
+    // data.room message를 보내는 room의 이름 (귓을 위해서는 socket_id를 room으로 사용하면 된다.)
     // data.user message를 보내는 user의 이름
     // data.message message : 메시지의 본문.
     let newMessage = new Message({
@@ -87,27 +98,20 @@ io.on("connection", (socket) => {
       if (err) console.log(err);
     });
     // Sending the new message to call the connected clients
-    console.log("newMessage : ",newMessage)
+    console.log("newMessage : ", newMessage);
     io.to(data.room).emit("new_message", newMessage);
   });
 });
 
 /*
-client test
-const socket = ios.connect('ws://localhost:3000', {transports: ['websocket']})
-let roomName="1";
-let data = {};
-data.value = "hello"
+클라이언트 이벤트
 socket.on("welcome", (data) => {
-  console.log("welcome socket")
-  console.log(data)
+  유저가 톡방을 들어왔을 때
+});
+socket.on("bye", (data) => {
+  유저가 톡방을 나갔을 때
 });
 
-socket.on("bye", () => {
-});
-
-socket.emit("new_message", (data) => {
-  console.log(`You: ${data.value}`);
 });
 */
 
