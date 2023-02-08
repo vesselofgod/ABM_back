3c1599f2bbdc390b48332ea6455972eb9d541908
vesselofgod <vesselofgod@naver.com>
2023-02-08 11:27:08 +0900

    feat: Add a chatting log GET API

--- routes/chat/chat.js
+++ routes/chat/chat.js
@@ -13,6 +13,7 @@ require("dotenv").config();
 
 router.get("/", async (req, res) => {
   try {
+    //유저가 속한 채팅방 list를 제공하는 API
     const token = req.header("authorization").split(" ")[1];
     const user_data = utils.parseJWTPayload(token);
     let rooms = await Room.find({
@@ -21,6 +22,24 @@ router.get("/", async (req, res) => {
     return res.status(200).json({
       success: true,
       rooms: rooms,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
+});
+
+router.get("/:room_id", async (req, res) => {
+  //채팅방 입장 시에 메시지 로그를 얻을 수 있는 API
+  try {
+    const room_id = req.params.room_id;
+    let chats = await Message.find({ room_id: room_id }).sort({ created: 1 });
+    return res.status(200).json({
+      success: true,
+      chats: chats,
     });
   } catch (err) {
     console.log(err);
